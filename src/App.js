55cdaf36203300663ce4f0fd0fbb7160075cf8e4
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-04-04 22:53:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Searchbar from "./components/Searchbar";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Searchbar />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (prop) => {
+  return (
+    <div className="header">
+      <a href="#">Gmail</a> 
+      <a href="#">Images</a> 
+      <img src ="https://lh3.googleusercontent.com/ogw/AAEL6sjr4yyFwZcQxpooKTEa5H-CPLkOHy2zn6dXFNUJ=s32-c-mo" alt="User initials"/>
+    </div>
+  )
+}
+
+export default Header
