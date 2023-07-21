642ae2472844107c6b5bd0e1c23082c968888df4
Asonwa Nnabeze <lilynnabeze@gmail.com>
2023-07-21 19:16:06 +0100

    footer links updated in the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,31 +14,39 @@ const FooterBottom = () => {
     <div className="footer-bottom">
       <ul className="footer-left">
         <li>
-          <a href="#">About</a>
+          <a href="https://about.google/">About</a>
         </li>
         <li>
-          <a href="#">Advertising</a>
+          <a href="https://ads.google.com/intl/en_ng/getstarted/?subid=ng-en-ha-aw-sk-c-bau!o3~Cj0KCQjw2eilBhCCARIsAG0Pf8uayZLE9vOfjRoXgbf6M8udpyCQilcHEyWYhuIfprmFn7sju5MiJwsaAoioEALw_wcB~149741567721~aud-1277764251247:kwd-436913726348~19700236792~648528764543&utm_source=aw&utm_medium=ha&utm_campaign=ng-en-ha-aw-sk-c-bau!o3~Cj0KCQjw2eilBhCCARIsAG0Pf8uayZLE9vOfjRoXgbf6M8udpyCQilcHEyWYhuIfprmFn7sju5MiJwsaAoioEALw_wcB~149741567721~aud-1277764251247:kwd-436913726348~19700236792~648528764543&gad=1&gclid=Cj0KCQjw2eilBhCCARIsAG0Pf8uayZLE9vOfjRoXgbf6M8udpyCQilcHEyWYhuIfprmFn7sju5MiJwsaAoioEALw_wcB&gclsrc=aw.ds">
+            Advertising
+          </a>
         </li>
         <li>
-          <a href="#">Business</a>
+          <a href="https://www.google.com/intl/en_ng/business/">Business</a>
         </li>
         <li>
-          <a href="#">How Search Works</a>
+          <a href="https://developers.google.com/search/docs/fundamentals/how-search-works#:~:text=Indexing%3A%20Google%20analyzes%20the%20text,relevant%20to%20the%20user's%20query.">
+            How Search Works
+          </a>
         </li>
       </ul>
       <div className="leaf-container">
         <img src={Leaf} alt="Leaf" className="leaf" />
-        <a href="#">Carbon neutral since 2007</a>
+        <a href="https://www.google.com/search?q=google+carbon+neutral+since+2007&oq=google+carbon+neutral&gs_lcrp=EgZjaHJvbWUqBwgBEAAYgAQyBggAEEUYOTIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIICAUQABgWGB4yCAgGEAAYFhgeMggIBxAAGBYYHjIICAgQABgWGB4yCAgJEAAYFhge0gEINTkwOGowajSoAgCwAgA&sourceid=chrome&ie=UTF-8">
+          Carbon neutral since 2007
+        </a>
       </div>
       <ul className="footer-right">
         <li>
-          <a href="#">Privacy</a>
+          <a href="https://policies.google.com/privacy?hl=en-US">Privacy</a>
         </li>
         <li>
-          <a href="#">Terms</a>
+          <a href="https://policies.google.com/terms?hl=en-US">Terms</a>
         </li>
         <li>
-          <a href="#">Settings</a>
+          <a href="https://support.google.com/accounts/answer/3118621?hl=en">
+            Settings
+          </a>
         </li>
       </ul>
     </div>
